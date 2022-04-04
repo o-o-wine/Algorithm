a4f3e11e35dc24d3f9a934124d9e2c622ecc16a1
GunHee.Lee <58353358+o-o-wine@users.noreply.github.com>
2022-04-04 22:02:21 +0900

    Update 미로 탐색.cc
    변수명 수정

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,19 +1,30 @@
 #include <iostream>
+#include <string>
+#include <stack>
 #include <queue>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <map>
+#include <cctype>
+#include <set>
+#include <string.h>
 
 using namespace std;
 
 int n, m;
-bool check[100][100]; // 방문 여부 저장
-int a[100][100];
-int length[100][100]; // 이동 거리 저장
-int dx[4] = {0, 0, 1, -1}; // x 축 이동 (열 이동)
-int dy[4] = {1, -1, 0, 0}; // y 축 이동 (행 이동)
+bool check[101][101]; 
+int mmap[101][101];
+int length[100][100];
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+queue<pair<int, int>> q;
 
-void bfs(int x, int y) {
-    queue<pair<int, int>> q;
+void BFS(int x, int y) {
     q.push(make_pair(y, x));
-    length[y][x] = 1;
+    //length[y][x] = 1;
     check[y][x] = true;
 
     while (!q.empty()) {
@@ -22,11 +33,11 @@ void bfs(int x, int y) {
         q.pop();
 
         for (int i = 0; i < 4; i++) {
-            int next_x = x + dx[i]; // 2. 이동 가능한 다음 좌표 계산
+            int next_x = x + dx[i]; 
             int next_y = y + dy[i];
 
-            if (next_x >= 0 && next_y >= 0 && next_x < m && next_y < n && a[next_y][next_x] == 1 && check[next_y][next_x] == false) {
-                length[next_y][next_x] = length[y][x] + 1; // 3. 총 이동 거리 계산
+            if (next_x >= 0 && next_y >= 0 && next_x < m && next_y < n && mmap[next_y][next_x] == 1 && check[next_y][next_x] == false) {
+                length[next_y][next_x] = length[y][x] + 1;
                 check[next_y][next_x] = true;
                 q.push(make_pair(next_y, next_x));
             }
@@ -35,12 +46,17 @@ void bfs(int x, int y) {
 }
 
 int main(void) {
+        ios_base::sync_with_stdio(false);
+	    cin.tie(NULL);
+	    cout.tie(NULL);
         scanf("%d %d", &n, &m);
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-                scanf("%1d", &a[j][k]); // 1. 붙어있는 수 나눠서 저장
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%1d", &mmap[i][j]);
             }
         }
-        bfs(0, 0);
-        printf("%d\n", length[n - 1][m - 1]);
+        
+        BFS(0, 0);
+    
+        cout << length[n - 1][m - 1] + 1 << '\n';
 }
