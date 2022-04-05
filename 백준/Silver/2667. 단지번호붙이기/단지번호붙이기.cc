1bb9b329e40b2715e6e2303d75a8ff95b544dccf
GunHee.Lee <58353358+o-o-wine@users.noreply.github.com>
2022-04-05 10:37:52 +0900

    Update 단지번호붙이기.cc

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -14,44 +14,30 @@ int house_number = 0;
 int map_data[MAX_SIZE][MAX_SIZE];
 bool visited[MAX_SIZE][MAX_SIZE];
  
- 
-// 우,하,좌,상
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
- 
- 
-// BFS
+
 void bfs(int y, int x) {
  
-    queue< pair<int, int> > q; // 이용할 큐, (x,y) -> (행, 열)
-    q.push(make_pair(y, x)); // pair를 만들어서 queue에 넣습니다.
+    queue< pair<int, int> > q; 
+    q.push(make_pair(y, x));
  
-    // 처음 x,y를 방문 했기때문에
     visited[y][x] = true;
     num_of_houses[house_number]++;
  
     while (!q.empty()) {
- 
-        // 큐의 현재 원소를 꺼내기
         y = q.front().first;
         x = q.front().second;
         q.pop();
- 
-        // 해당 위치의 주변을 확인
+        
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
- 
-            // 지도를 벗어나지 않고
+
             if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                // 집이면서 방문하지 않았다면 -> 방문
                 if (map_data[ny][nx] == 1 && visited[ny][nx] == false) {
                     visited[ny][nx] = true;
- 
-                    // 해당 단지의 집의 수를 증가시킴
                     num_of_houses[house_number]++;
- 
-                    // 큐에 현재 nx,ny를 추가
                     q.push(make_pair(ny, nx));
                 }
             }
@@ -62,30 +48,23 @@ void bfs(int y, int x) {
 int main() {
     scanf("%d", &n);
  
-    // 지도 데이터 입력
     for (int col = 0; col < n; col++) {
         for (int raw = 0; raw < n; raw++) {
             scanf("%1d", &map_data[col][raw]);
         }
     }
- 
-    // 전체의 지도 데이터를 하나하나 체크
+
     for (int col = 0; col < n; col++) {
         for (int raw = 0; raw < n; raw++) {
-            // 집이 존재하고, 방문하지 않았을 때,
             if (map_data[col][raw] == 1 && visited[col][raw] == false) {
                 house_number++;
- 
-                // bfs 탐색 진행
                 bfs(col, raw);
             }
         }
     }
- 
-    // 오름차순으로 정렬
+
     sort(num_of_houses + 1, num_of_houses + house_number + 1);
  
-    // 출력
     printf("%d\n", house_number);
     for (int i = 1; i <= house_number; i++) {
         printf("%d\n", num_of_houses[i]);
@@ -94,4 +73,5 @@ int main() {
 }
  
 
+ 
 
